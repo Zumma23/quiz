583139cc21d0f42792c26c88e2ec4f40320182a6
Zumma23 <83497168+Zumma23@users.noreply.github.com>
2021-06-25 15:37:36 +0300

    update request

--- client/src/pages/Game/Game.tsx
+++ client/src/pages/Game/Game.tsx
@@ -2,23 +2,18 @@ import React, { FC, useState, useEffect } from "react";
 import axios from "axios";
 import Quiz from "../../components/Quiz/Quiz";
 const Game: FC = () => {
-
   const [questions, setQuestions] = useState();
 
-
   useEffect(() => {
-    axios.get("http://localhost:5000/api/find").then((res) => {
+    axios.get("/api/find").then((res) => {
       setQuestions(res.data);
     });
-
   }, []);
-
-
 
   return (
     <>
       <h1>Game</h1>
-      <Quiz questions={questions} setQuestions={setQuestions}/>
+      <Quiz questions={questions} setQuestions={setQuestions} />
     </>
   );
 };
